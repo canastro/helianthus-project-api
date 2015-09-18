2aac25b27f5299956148f1f279ab1b4892fb3158
Ricardo Canastro <ricardocanastro@gmail.com>
2015-09-18 21:31:08 +0100

    Added album model

---
+++ app/models/album.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var Photo = require('./photo');
+
+var AlbumSchema = new Schema({
+    name: String,
+    title: String,
+    shortDescription: String,
+    story: String,
+    photos: [{ type: ObjectId, ref: 'Photo' }]
+});
+
+module.exports = mongoose.model('Album', AlbumSchema);
